1c1b2949d30598051f84618dddaea96672915e0a
golubexe <simon.cv@hotmail.com>
2022-10-25 23:10:27 +0200

    Update

--- LabVezzba/main.c
+++ LabVezzba/main.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void sporedba(int a, int b, float c, float d) {
 
+  if (a > b && c > d) {
+    printf("Najgolemiot cel broj e %d || Najgolemiot realen broj e %.2f", a, c);
 
-int main()
-{
-    int broj1, broj2;
-    float broj3, broj4;
+  } else if (b > a && d > c) {
+    printf("Najgolemiot broj e %d || Najgolemiot realen broj e %.2f", b, d);
 
-    printf("Vnesete dva celi broja, pa dva realni broja\n");
-    scanf("%d %d %f %f", &broj1, &broj2, &broj3, &broj4);
+  } else {
+    printf("Broevite se ednakvi");
+  }
+}
 
-    printf("Zbirot od dvata celi broja iznesuva %d || Zbirot od dvata realni broja iznesuva %.2f\n", broj1+broj2, broj3+broj4);
-    printf("Razlikata od dvata broja iznesuva %d || Razlika od dvata realni broja iznesuva %.2f\n", broj1-broj2, broj3-broj4);
-    printf("Proizvodot od dvata broja iznesuva %d || Proizvod od dvata realni broja iznesuva %.2f\n", broj1*broj2, broj3*broj4);
+int main() {
+  int broj1, broj2;
+  float broj3, broj4;
 
-    if(broj1 > broj2 && broj3 > broj4){
-            printf("Najgolemiot cel broj e %d || Najgolemiot realen broj e %.2f", broj1, broj3);
-        }
-        else if (broj2 > broj1 && broj4 > broj3){
-            printf("Najgolemiot broj e %d || Najgolemiot realen broj e %.2f", broj2, broj4);
-        }
-        else{
-            printf("Broevite se ednakvi");
-        }
+  printf("Vnesete dva celi broja, pa dva realni broja\n");
+  scanf("%d %d %f %f", & broj1, & broj2, & broj3, & broj4);
 
-    return 0;
+  printf("Zbirot od dvata celi broja iznesuva %d || Zbirot od dvata realni broja iznesuva %.2f\n", broj1 + broj2, broj3 + broj4);
+  printf("Razlikata od dvata broja iznesuva %d || Razlika od dvata realni broja iznesuva %.2f\n", broj1 - broj2, broj3 - broj4);
+  printf("Proizvodot od dvata broja iznesuva %d || Proizvod od dvata realni broja iznesuva %.2f\n", broj1 * broj2, broj3 * broj4);
+
+  sporedba(broj1, broj2, broj3, broj4);
+
+  return 0;
 }
