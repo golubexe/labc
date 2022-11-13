e481f113acfa5aa6d76adf4afa36ce2873ed374c
golubexe <cvetanovski72@gmail.com>
2022-11-13 23:44:37 +0100

    Doner ( Rekurzija 2 )

---
+++ Plaset2/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int funkcija (int n)
+{
+    if (n != 0){
+        while (((n / 10) % 10) < n % 10) {
+            return n < 9 ?  1 : funkcija (n /= 10);
+    	}
+    }
+    return 0;
+}
+int main ()
+{
+  int n;
+  printf ("Vnesi broj : ");
+  scanf ("%d", &n);
+  printf ("%d", funkcija (n));
+  return 0;
+}
+
